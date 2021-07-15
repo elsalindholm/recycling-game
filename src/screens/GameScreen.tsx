ad25ed7c1cf0de5b65b45e1211a26a6d271b07aa
elsalindholm <31487907+elsalindholm@users.noreply.github.com>
2021-07-15 16:28:30 +0100

    grid items placed on a circle

--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -15,12 +15,24 @@ export class GameScreen extends React.PureComponent<GameScreenProps> {
 
     return (
       <div className={'game-screen'}>
-        <div className={'game-screen-navbar'}>
-          <button className={'game-screen-button'} onClick={() => endGame()}>
-            Exit Game
-          </button>
+        <div className={'game-grid'}>
+          <div className={'exit-button-container'}>
+            <button className={'game-screen-button'} onClick={() => endGame()}>
+              Exit Game
+            </button>
+          </div>
+          <div className={'grid-item bin1'}>Bin 1</div>
+          <div className={' grid-item bin2'}>Bin 2</div>
+          <div className={' grid-item bin3'}>Bin 3</div>
+          <div className={'grid-item bin4'}>Bin 4</div>
+          <div className={' grid-item bin5'}>Bin 5</div>
+          <div className={'grid-item bin6'}>Bin 6</div>
+          <div className={'grid-item bin7'}>Bin 7</div>
+          <div className={'grid-item bin8'}>Bin 8</div>
+          <div className={'grid-item item-to-sort'}>Item to sort</div>
+          <div className={'grid-item score'}>Score</div>
+          <div className={'grid-item item-sort-counter'}>Items to sort counter</div>
         </div>
-        <div className={'game-grid'}></div>
       </div>
     );
   }
